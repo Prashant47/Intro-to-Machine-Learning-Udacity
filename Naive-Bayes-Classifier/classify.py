034eaaf07acdbded9fc662a8550f11b0457e6e2a
Prashant Gonarkar <prashantgonarkar@gmail.com>
2016-01-23 14:42:41 +0530

    added files for calculating accuracy of prediction

---
+++ Naive-Bayes-Classifier/classify.py
@@ -0,0 +1,26 @@
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+
+    ### create classifier
+    clf = GaussianNB()
+    
+
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+    
+
+    ### use the trained classifier to predict labels for the test features
+    pred = clf.predict(features_test)
+
+
+    ### calculate and return the accuracy on the test data
+    ### this is slightly different than the example, 
+    ### where we just print the accuracy
+    ### you might need to import an sklearn module
+    intersect = [i for i, j in zip(pred, labels_test) if i == j]
+    matched = len(intersect)
+    total = len(labels_test)
+    accuracy = float(matched) / float(total)
+    return accuracy
